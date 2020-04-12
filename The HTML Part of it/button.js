056460bea59e749833dd1a783570a7e55897e70f
brandoge91 <63559800+brandoge91@users.noreply.github.com>
2020-04-12 15:46:56 -0400

    initial commit

---
+++ The HTML Part of it/button.js
@@ -0,0 +1,4 @@
+function redirectToSite(params) {
+    alert("You Are Now Being Redirected to the Official Troop 104 Website! Hit OK to Continue!");
+    window.open('https://www.troop104.com', '_blank')
+}
